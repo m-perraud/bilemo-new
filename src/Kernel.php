46275aefcd19d37349bc917fd689bfd03cf4e3f0
Melody Perraud <melody.perraud@gmail.com>
2023-05-10 14:13:41 +0200

    Paramétrage documentation

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,24 @@
 
 namespace App;
 
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Nelmio\ApiDocBundle\NelmioApiDocBundle;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    /*
+    public function registerBundles(): iterable
+    {
+        $bundles = [
+            // ...
+
+            new NelmioApiDocBundle(),
+        ];
+
+        // ...
+    }
+    */
 }
